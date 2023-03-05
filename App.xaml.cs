8f97b0d7da1b6acb149cbb09f37649d5e50c5799
Vladyslav Zabrodskyi <vladyslav.zabrodskyi@fordewind.io>
2023-03-05 11:51:33 +0200

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KMA.ProgrammingInCSharp2023.Lab01.DateApp
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Views/InputControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using KMA.ProgrammingInCSharp2023.Lab01.DateApp.ViewModels;
+
+namespace KMA.ProgrammingInCSharp2023.Lab01.DateApp
+{
+    public partial class InputControl:UserControl
+    {
+        public InputControl()
+        {
+            InitializeComponent();
+            DataContext = new InputDateViewModel();
+        }
+    }
+}
